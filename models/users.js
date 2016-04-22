610ae6648947ecb207018ec0565c567e1a013f8e
Henrique Melo <hpa.de.melo@gmail.com>
2016-04-22 00:59:21 -0400

    Testing DB

--- models/users.js
+++ models/users.js
@@ -22,14 +22,14 @@ var userSchema = new mongoose.Schema({
 var User = mongoose.model('User', userSchema);
 
 // Create a unique instance of the User model. Set its properties.
-/*var henrique = new User({name: 'Henrique', email: 'hpa.de.melo@gmail.com', password: '1234'});
+var henrique = new User({name: 'Henrique', email: 'hpa.de.melo@gmail.com', password: '1234'});
 
-User.find({}, function(err, allUsers) {
+/*User.find({}, function(err, allUsers) {
   if (err) {
     console.error(err);
   } else {
     console.log('These are all users:\n' + allUsers);
   }
-});*/
-
+});
+*/
 module.exports = User;
