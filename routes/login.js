3cdc28ef0845b671e53603374c229e6924843955
Henrique Melo <hpa.de.melo@gmail.com>
2016-04-22 12:24:22 -0400

    Heroku Issue Fixed

--- routes/login.js
+++ routes/login.js
@@ -7,21 +7,21 @@
 // What if the email is not in the db?
 // What is the pwd is wrong?
 // Handle this later.
-/*var rightPwd = function (userEmail, pwd, callback) {
-    // if email === email in database and pwd === pwd in database return true
+// var rightPwd = function (userEmail, pwd, callback) {
+//     // if email === email in database and pwd === pwd in database return true
 
-    User.findOne({ email: userEmail }, function (err, obj) {
-        if (!obj || err) {
-            callback(err, false);
-        } else {
-            if (pwd === obj.password) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    });
-};*/
+//     User.findOne({ email: userEmail }, function (err, obj) {
+//         if (!obj || err) {
+//             callback(err, false);
+//         } else {
+//             if (pwd === obj.password) {
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     });
+// };
 
 module.exports = function(app, passport) {
 
