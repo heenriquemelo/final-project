79cc4dde997adf373cee31bd7a3e0f3062423b77
Henrique Melo <hpa.de.melo@gmail.com>
2016-04-21 23:28:38 -0400

    Changed DB config

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ var db = mongoose.connection;
 
 // Establishes connection to db. If run locally, u must create a new terminal window, and run the db from there.
 // The command "mongod --port <port number>" takes care of the work.
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:12345');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:12345');
 
 // Once the connection is made, execute the callback.
 db.once('open', function() {
