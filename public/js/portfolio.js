d8229441a3220a271aa36fa12f227b1f92a1c5a2
Henrique Melo <hpa.de.melo@gmail.com>
2016-04-29 17:57:18 -0400

    Current price and return now being displayed in the portfolio table

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -2,15 +2,32 @@ $(function () {
     $.get('/stockslist', function (stocks) {
         // Use EJS template to display ONE stock.
         // Iterate through this template using a for loop for each stock.
+        console.time('test');
+        var count = 0;
         for (var i = 0; i < stocks.length; i++) {
             var params = {symbol: stocks[i].symbol};
             $.get('/stockdata', params, function (data) {
-                stocks[i][currentPrice] = data.lastPrice;
+                for (var x = 0; x < stocks.length; x++) {
+                    if (stocks[x].name === data.name) {
+                        stocks[x].currentPrice = data.lastPrice;
+                        if (stocks[x].currentPrice > stocks[x].priceBought) {
+                            var percentReturn = '+ ' + ((stocks[x].currentPrice/stocks[x].priceBought - 1)*100).toFixed(2);
+                        } else if (stocks[x].currentPrice == stocks[x].priceBought) {
+                            var percentReturn = 0.00;
+                        } else {
+                            var percentReturn = '- ' + ((1 - stocks[x].currentPrice/stocks[x].priceBought)*100).toFixed(2);
+                        }
+                        stocks[x].percentReturn = percentReturn;
+                    }
+                }
+                if (++count === stocks.length) {
+                    // DONT PUT GET INSIDE A FOR LOOP
+                    console.log(stocks);
+                    var template = new EJS({url: '../public/js/portfolio-template.ejs'});
+                    template.update('stocks-bought', {stocks: stocks});
+                }
             });
+        console.timeEnd('test');
         }
-        // DONT PUT GET INSIDE A FOR LOOP
-        console.log(stocks);
-        var template = new EJS({url: '../public/js/portfolio-template.ejs'});
-        template.update('stocks-bought', {stocks: stocks});
     });
 });
